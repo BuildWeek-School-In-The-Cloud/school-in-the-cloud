777303cf6c70b7f20cd60c6ca25937714829ee07
hans0 <kiessler.hans@gmail.com>
2021-03-01 16:06:39 -0500

    Bringing stuff over

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page-wrapper">
       <header id="header" class="alt">
         <h1><a href="index.html">School In The Cloud</a></h1>
         <nav id="nav">
@@ -23,6 +23,55 @@ function App() {
           </ul>
         </nav>
       </header>
+      {/* Banner */}
+      <section id="banner">
+        <div class="inner">
+          <h2>School In The Cloud</h2>
+          <p>
+            School in the Cloud is a platform that trains senior volunteers to teach students in a group or individual setting.
+          </p>
+          <ul class="actions special">
+            <li><a href="#" class="button primary">Explore</a></li>
+          </ul>
+        </div>
+        <a href="#one" class="more scrolly">Learn More</a>
+      </section>
+      {/* One */}
+      <section id="one" class="wrapper style1 special">
+        <div class="inner">
+          <header class="major">
+            <h2>
+              Arcu aliquet vel lobortis ata nisl<br />
+              eget augue amet aliquet nisl cep donec
+            </h2>
+            <p>
+              Aliquam ut ex ut augue consectetur interdum. Donec amet imperdiet
+              eleifend<br />
+              fringilla tincidunt. Nullam dui leo Aenean mi ligula, rhoncus
+              ullamcorper.
+            </p>
+          </header>
+          <ul class="icons major">
+            <li>
+              <span class="icon fa-gem major style1"
+                ><span class="label">Lorem</span></span
+              >
+            </li>
+            <li>
+              <span class="icon fa-heart major style2"
+                ><span class="label">Ipsum</span></span
+              >
+            </li>
+            <li>
+              <span class="icon solid fa-code major style3"
+                ><span class="label">Dolor</span></span
+              >
+            </li>
+          </ul>
+        </div>
+      </section>
+
+
     </div>
   );
 }
