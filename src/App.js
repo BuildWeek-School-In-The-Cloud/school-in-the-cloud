3e32ba8e3eec45831d25f78dfb69eb01c394325e
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-03-01 22:07:17 -0600

    Finished blocking out the HTML, will tackle styling using Styled Components in the morning

--- src/App.js
+++ src/App.js
@@ -73,10 +73,159 @@ function App() {
       <section id="two">
         <section className="spotlight">
           <div className="img">
-            <img src="images.pic01.jpg" alt="" />
+            <img src="../images/pic01" alt="" />
+          </div>
+          <div className="content">
+            <h2>MAGNA PRIMIS LOBORTIS SED ULLAMCORPER</h2>
+            <p>
+              Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
+              imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+            </p>
+          </div>
+        </section>
+        <section className="spotlight">
+          <div className="img">
+            <img src="./images.pic02" alt="" />
+          </div>
+          <div className="content">
+            <h2> TORTOR DOLORE FEUGIAT ELEMENTUM MAGNA </h2>
+            <p>
+              {" "}
+              Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
+              imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.
+            </p>
+          </div>
+        </section>
+        <section className="spotlight">
+          <div className="img">
+            <img src="./images/pic03" alt="" />
+          </div>
+          <div className="content">
+            <h2> AUGUE ELEIFEND ALIQUET SED CONDIMENTUM </h2>
+            <p>
+              {" "}
+              Aliquam ut ex ut augue consectetur interdum. Donec hendrerit
+              imperdiet. Mauris eleifend fringilla nullam aenean mi ligula.{" "}
+            </p>
           </div>
         </section>
       </section>
+      <section id="three">
+        <div className="inner">
+          <header>
+            <h2>ACCUMSAN MUS TORTOR NUNC ALIQUET</h2>
+            <p>
+              ACCUMSAN MUS TORTOR NUNC ALIQUET Aliquam ut ex ut augue
+              consectetur interdum. Donec amet imperdiet eleifend fringilla
+              tincidunt. <br></br>
+              Nullam dui leo Aenean mi ligula, rhoncus ullamcorper.
+            </p>
+          </header>
+          <ul className="features">
+            <li className="icon paper-plane">
+              <h3> ARCU ACCUMSAN </h3>
+              <p>
+                {" "}
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.{" "}
+              </p>
+            </li>
+            <li className="icon laptop">
+              <h3> AC AUGUE EGET </h3>
+              <p>
+                {" "}
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.{" "}
+              </p>
+            </li>
+            <li className="icon code">
+              <h3> MUS SCELERISQUE </h3>
+              <p>
+                {" "}
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.{" "}
+              </p>
+            </li>
+            <li className="icon headphone">
+              <h3> MAURIS IMPERDIET </h3>
+              <p>
+                {" "}
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.{" "}
+              </p>
+            </li>
+            <li className="icon heart">
+              <h3> AENEAN PRIMIS </h3>
+              <p>
+                {" "}
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.{" "}
+              </p>
+            </li>
+            <li className="icon flag">
+              <h3> TORTOR UT </h3>
+              <p>
+                {" "}
+                Augue consectetur sed interdum imperdiet et ipsum. Mauris lorem
+                tincidunt nullam amet leo Aenean ligula consequat consequat.{" "}
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <section id="cta">
+        <div className="inner">
+          <header>
+            <h2> ARCUE UT VEL COMMODO </h2>
+          </header>
+          <p>
+            {" "}
+            Aliquam ut ex ut augue consectetur interdum endrerit imperdiet amet
+            eleifend fringilla.{" "}
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#explore" className="button">
+                EXPLORE
+              </a>
+            </li>
+            <li>
+              <a href="#learnMore" className="button">
+                LEARN MORE
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <footer id="footer">
+        <ul className="icons">
+          <li>
+            <a href="#twitter" className="icon twitter">
+              <span className="label">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a href="#facebook" className="icon facebook">
+              <span className="label">facebook</span>
+            </a>
+          </li>
+          <li>
+            <a href="#instagram" className="icon instagram">
+              <span className="label">instagram</span>
+            </a>
+          </li>
+          <li>
+            <a href="#dribble" className="icon dribble">
+              <span className="label">dribble</span>
+            </a>
+          </li>
+          <li>
+            <a href="#email" className="icon envelope">
+              <span className="label">email</span>
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
