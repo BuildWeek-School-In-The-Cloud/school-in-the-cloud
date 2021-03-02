c9c6dff032e86139b355e28f2c947a997015e67a
Chance Swartz <71352443+chance10113@users.noreply.github.com>
2021-03-02 13:23:07 -0600

    playing around with images, can link them through Unsplash

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
       <section id="two">
         <section className="spotlight">
           <div className="img">
-            <img src="../images/pic01" alt="" />
+            <img src="https://images.unsplash.com/photo-1461532257246-777de18cd58b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1355&q=80" alt="" />
           </div>
           <div className="content">
             <h2>MAGNA PRIMIS LOBORTIS SED ULLAMCORPER</h2>
